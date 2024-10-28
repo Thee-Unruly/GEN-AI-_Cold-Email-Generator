b75b4e0b9086dc820a0fb51e750320fa5e9028be
Ibrahim Karisa Fadhili <ibrahim.fadhili@agilebiz.co.ke>
2024-10-28 13:38:05 +0300

    extract info from a website

--- gen_email.py
+++ gen_email.py
@@ -6,5 +6,37 @@ llm = ChatGroq(
     temperature=0
 )
 
-response = llm.invoke('Who is Lupita Nyongo')
-print (response)
+#response = llm.invoke('')
+#print (response)
+
+from langchain_community.document_loaders import WebBaseLoader
+
+loader = WebBaseLoader("https://jobs.nike.com/job/R-40807?from=job%20search%20funnel")
+
+page_data = loader.load().pop().page_content
+#print(page_data)
+
+from langchain_core.prompts import PromptTemplate
+
+prompt_extract = PromptTemplate.from_template(
+        """
+        ### SCRAPED TEXT FROM WEBSITE:
+        {page_data}
+        ### INSTRUCTION:
+        The scraped text is from the career's page of a website.
+        Your job is to extract the job postings and return them in JSON format containing the 
+        following keys: `role`, `experience`, `skills` and `description`.
+        Only return the valid JSON.
+        ### VALID JSON (NO PREAMBLE):    
+        """
+)
+
+chain_extract = prompt_extract | llm 
+res = chain_extract.invoke(input={'page_data':page_data})
+res.content
+
+from langchain_core.output_parsers import JsonOutputParser
+
+json_parser = JsonOutputParser()
+
+json_res = json_parser.parse(res.content)
